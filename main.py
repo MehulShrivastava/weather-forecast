d8051e546fd696e512c4e53fd2dc383a524119ef
MehulShrivastava <mehulshrivastava72@gmail.com>
2023-08-06 04:09:04 +0530

    completed

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5, help='Select the number of forecast days')
-options = st.selectbox("Select data to view", ('Temperature', 'Sky'))
-st.subheader(f"{options} for the next {days} days in {place}")
+days = st.slider("Forecast Days", min_value=1, max_value=5,
+                 help='Select the number of forecast days')
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
 
 
-def get_data(days):
-    dates = ["2023-28-07", "2023-27-07", "2023-22-07"]
-    temperatures = [10, 15, 20]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+if place:
+    # Get the temperature/sky data
+    try:
+        filtered_data = get_data(place, days)
 
-
-d, t = get_data(days)
-
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature(C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict1["main"]["temp"] / 10 for dict1 in filtered_data]
+            dates = [dict1["dt_txt"] for dict1 in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict1["weather"][0]["main"] for dict1 in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("That place does not exist.")
